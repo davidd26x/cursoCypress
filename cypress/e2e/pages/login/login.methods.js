b4732f708f0fb2abe2daba42739f535e73a3e902
davidd26x <jose01265@gmail.com>
2025-05-08 09:12:01 -0600

    Fix report and flakyness

--- cypress/e2e/pages/login/login.methods.js
+++ cypress/e2e/pages/login/login.methods.js
@@ -1,12 +1,13 @@
 import { LoginElements } from "./login.elements";
 
+//Se usa el invoke en vez del type para evitar el delay de escritura y que se completen los campos correctamente
 export class LoginMethods{
     static insertarUsername(username){
-        LoginElements.textboxes.username.type(username)
+        LoginElements.textboxes.username.invoke('val', username)
     }
 
     static insertarPassword(password){
-        LoginElements.textboxes.password.type(password)
+        LoginElements.textboxes.password.invoke('val', password)
     }
 
     static clickLoginButton(){
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,31 @@
-import { LoginElements } from "./pages/login/login.elements"
-import { LoginMethods } from "./pages/login/login.methods"
+import { LoginElements } from "./pages/login/login.elements";
+import { LoginMethods } from "./pages/login/login.methods";
 
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://demoblaze.com/')
+describe("template spec", () => {
+  it("passes", () => {
+    //Se define un usuario y password
+    const usuario = "random01";
+    const password = "random01";
 
-    //Hacer click en el boton de login
-    cy.get('a[data-target="#logInModal"]').click()
+    //Visitar la pagina de demoblaze
+    cy.visit("https://demoblaze.com/");
 
-    //Llamada de los metodos
-    LoginMethods.loginGlobal('username', 'password')
-    cy.wait(5000)
-  })
-})
+    //Click en el boton de login
+    cy.get('a[data-target="#logInModal"]').click();
+
+    //Llamada de metodo Login
+    LoginMethods.loginGlobal(usuario, password);
+
+    //Validar que el usuario se haya logueado correctamente
+    cy.get("a#nameofuser").should("contain.text", usuario);
+
+    cy.wait(10000);
+
+    // //Hacer click en el boton de login
+    // cy.get('a[data-target="#logInModal"]').click()
+
+    // //Llamada de los metodos
+    // LoginMethods.loginGlobal('username', 'password')
+    // cy.wait(5000)
+  });
+});
