7cd2947f721df26c9645923eca69125cbf6d0e68
davidd26x <jose01265@gmail.com>
2025-05-08 10:45:47 -0600

    Creacion de cart y detalles del producto

--- cypress/e2e/pages/product-details/product-details.elements.js
+++ cypress/e2e/pages/product-details/product-details.elements.js
@@ -2,7 +2,7 @@ export class ProductDetailsElements {
   static get buttons() {
     return {
       get addToCartButton() {
-        cy.contains("a", "Add to cart");
+        return cy.contains("a", "Add to cart");
       },
     };
   }
