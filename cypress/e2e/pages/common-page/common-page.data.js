e07e933a8b41e5b1685191d1b6d6826f8fa1e194
davidd26x <jose01265@gmail.com>
2025-05-09 09:10:23 -0600

    Creacion de Parte Common page

---
+++ cypress/e2e/pages/common-page/common-page.data.js
@@ -0,0 +1,6 @@
+export class CommonPageData {
+  //Se agarra el link de la página
+  static get url() {
+    return "https://demoblaze.com/cart.html";
+  }
+}
---
+++ cypress/e2e/pages/common-page/common-page.methods.js
@@ -0,0 +1,39 @@
+import { CommonPageData } from "./common-page.data";
+import { CommonPageElements } from "./common-page.elements";
+
+export class CommonPageMethods {
+  //Metodos que naveguen a la página
+  static navigateToDemoBlaze() {
+    cy.visit(CommonPageData.url);
+  }
+
+  static clickOnHomeOption() {
+    //Al hacer click en la seccion Home del Menú de arriba
+    CommonPageElements.topMenu.home.click();
+  }
+
+  static clickOnContactOption() {
+    //Al hacer click en la seccion Contact del Menú de arriba
+    CommonPageElements.topMenu.contact.click();
+  }
+
+  static clickOnAboutUsOption() {
+    //Al hacer click en la seccion About Us del Menú de arriba
+    CommonPageElements.topMenu.aboutUs.click();
+  }
+
+  static clickOnCartOption() {
+    //Al hacer click en la seccion Cart del Menú de arriba
+    CommonPageElements.topMenu.cart.click();
+  }
+
+  static clickOnLogInOption() {
+    //Al hacer click en la seccion Log In del Menú de arriba
+    CommonPageElements.topMenu.logIn.click();
+  }
+
+  static clickOnSignUpOption() {
+    //Al hacer click en la seccion Sign Up del Menú de arriba
+    CommonPageElements.topMenu.signUp.click();
+  }
+}
